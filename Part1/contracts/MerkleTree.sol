f78fadfdb777ae7280b5da84ce2c0a932402b65e
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-05-26 13:45:02 -0400

    basic modifications to get the project up and running

--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -4,13 +4,21 @@ pragma solidity ^0.8.0;
 import { PoseidonT3 } from "./Poseidon.sol"; //an existing library to perform Poseidon hash on solidity
 import "./verifier.sol"; //inherits with the MerkleTreeInclusionProof verifier contract
 
-contract MerkleTree is Verifier {
+contract MerkleTree is Groth16Verifier {
     uint256[] public hashes; // the Merkle tree in flattened array form
     uint256 public index = 0; // the current index of the first unfilled leaf
     uint256 public root; // the current Merkle root
 
     constructor() {
         // [assignment] initialize a Merkle tree of 8 with blank leaves
+        index = 9;
+        for (uint i = 0; i < 15; i++) {
+            hashes.push(0);
+        }
+        for (uint i = 6; i >=0; i--) {
+            hashes[i] = PoseidonT3.poseidon([hashes[i*2], hashes[i*2+1]]);
+        }
+        root = hashes[0];
     }
 
     function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
--- Part1/scripts/bump-solidity.js
+++ Part1/scripts/bump-solidity.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const solidityRegex = /pragma solidity \^\d+\.\d+\.\d+/
 
-let content = fs.readFileSync("./contracts/verifier.sol", { encoding: 'utf-8' });
+let content = fs.readFileSync("./Part1/contracts/verifier.sol", { encoding: 'utf-8' });
 let bumped = content.replace(solidityRegex, 'pragma solidity ^0.8.0');
 
-fs.writeFileSync("./contracts/verifier.sol", bumped);
+fs.writeFileSync("./Part1/contracts/verifier.sol", bumped);
